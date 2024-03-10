2363ebd60746de7d8b3d09c0cf41a90452e5f797
Luiz Felipe <luizfelipe.consultoria20@gmail.com>
2024-03-10 01:49:21 -0300

    update

---
+++ teste.js
@@ -0,0 +1,9 @@
+const numbers = [1, 2, 3, 4, 5];
+const doubledNumbers = numbers.map((num) => num * 2);
+// doubledNumbers será [2, 4, 6, 8, 10]
+
+const numbers2 = [1, 2, 3, 4, 5];
+numbers2.forEach((num) => console.log(num * 2));
+// Isso irá imprimir o dobro de cada número no console
+
+console.log(doubledNumbers)
